d13ff9bee8e79ba5a1dab01727f6ed2aa87320ac
abyssian03 <abyssian03@gmail.com>
2025-01-08 18:19:54 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -12,6 +12,15 @@ def initiate_db():
     price INTEGER NOT NULL)
     ''')
 
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL)
+    ''')
+
     # descript = ["Витаминный комплекс", "Минеральный комплекс", "Ноотропный комплекс", "Комплекс для здорового сна"]
     # for i in range(4):
     #     cursor.execute("INSERT INTO Products (id, title, description, price) VALUES (?, ?, ?, ?)",
@@ -28,3 +37,22 @@ def get_all_products():
     connection.commit()
     connection.close()
     return(products)
+
+def is_included(username):
+    connection = sqlite3.connect("for_telegram.db")
+    cursor = connection.cursor()
+    user = cursor.execute("SELECT * FROM Users WHERE username=?", (username,))
+    connection.commit()
+    if user.fetchone() is None:
+        connection.close()
+        return False
+    else:
+        connection.close()
+        return True
+
+def add_user(username, email, age):
+    connection = sqlite3.connect("for_telegram.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",(f'{username}', f'{email}', f'{age}', "1000"))
+    connection.commit()
+    connection.close()
